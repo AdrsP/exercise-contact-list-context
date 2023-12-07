de99cd048568e6428315513d733e543316632add
AdrsP <144963769+AdrsP@users.noreply.github.com>
2023-12-07 02:36:58 +0000

    agregue la funcion al boton y actualizo la api

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -3,33 +3,40 @@ const getState = ({ getStore, setStore }) => {
 		store: {
 			//Your data structures, A.K.A Entities
 			//Aqui vamos a guarda todos los estados globales de nuestra app
-			contact: [],
-				},
+			contact: []
+		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
-			agregarContacto: (name, email, phone, address) =>{
-			//objeto con la informacion para enviar en el fetch
-			let datos = {
-				full_name: name,
-                email: email,
-                agenda_slug: "Andres", // nombre de la agenda 
-                address: address,
-                phone: phone
+			agregarContacto: (name, email, phone, address) => {
+				//objeto con la informacion para enviar en el fetch, probablemente la use con el boton save
+				let datos = {
+					full_name: name,
+					email: email,
+					agenda_slug: "Andres", // nombre de la agenda se permanece constante es un identificador impuesto por la api que debe definirse por el usuario
+					address: address,
+					phone: phone
+				};
+				fetch("https://playground.4geeks.com/apis/fake/contact/", {
+					method: "POST",
+					body: JSON.stringify(datos),
+					headers: { "Content-Type": "application/json" }
+				})
+					.then(response => response.json())
+					.then(data => console.log(data))
+					.catch(error => console.log(error)); //
+			},
+			actualizarContactStore: () => {
+				//busco que el store se actualice con la api deberia usar un fetch
+				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/Andres")
+					.then(response => response.json())
+					.then(data => setStore({contact: data})
 			}
-			fetch("https://playground.4geeks.com/apis/fake/contact/",{
-				method: "POST",
-				body: JSON.stringify(datos),
-				headers: {"Content-Type":"application/json"}
-			})
-			.then(response => response.json())
-			.then(data => console.log(data))
-			.catch(error => console.log(error))  //
-			}, 
 		}
 	};
 };
 
-// .then(data => setStore({contact: data})) store.contact
+// .then(data => setStore({contact: data})) store.contact datos para el get
+// necesito actualizar el contact
 
 export default getState;
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,17 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import {Context} from "../store/appContext.js"
-
+import { Context } from "../store/appContext.js";
 
 export const AddContact = () => {
+	const { actions, store } = useContext(Context);
 
-	const {actions, store} = useContext(Context);
-
-	function saveContact (){
-		actions.agregarContacto(/*meter use states de tlf, direccion, etc*/)
+	// hooks para los inputs intentar hacer esto despues con un solo hook que tenga un objeto para ahorrar codigo
+	const [inputName, setInputName] = useState("");
+	const [inputEmail, setInputEmail] = useState("");
+	const [inputPhone, setInputPhone] = useState("");
+	const [inputAddress, setInputAddres] = useState("");
+	// fucion para usar en el boton save
+	function saveContact() {
+		actions.agregarContacto(inputName, inputEmail, inputPhone, inputAddress);
+		setInputName("");
+		setInputEmail("");
+		setInputPhone("");
+		setInputAddres("");
+		actions.actualizarContactStore();
 	}
 
-// realizar un map con el store store.contact.map(...)
+	// realizar un map con el store store.contact.map(...)
 
 	return (
 		<div className="container">
@@ -20,21 +29,45 @@ export const AddContact = () => {
 				<form>
 					<div className="form-group">
 						<label>Full Name</label>
-						<input type="text" className="form-control" placeholder="Full Name" />
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Full Name"
+							value={inputName}
+							onChange={e => setInputName(e.target.value)}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Email</label>
-						<input type="email" className="form-control" placeholder="Enter email" />
+						<input
+							type="email"
+							className="form-control"
+							placeholder="Enter email"
+							value={inputEmail}
+							onChange={e => setInputEmail(e.target.value)}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Phone</label>
-						<input type="phone" className="form-control" placeholder="Enter phone" />
+						<input
+							type="phone"
+							className="form-control"
+							placeholder="Enter phone"
+							value={inputPhone}
+							onChange={e => setInputPhone(e.target.value)}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Address</label>
-						<input type="text" className="form-control" placeholder="Enter address" />
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter address"
+							value={inputAddress}
+							onChange={e => setInputAddres(e.target.value)}
+						/>
 					</div>
-					<button type="button" className="btn btn-primary form-control">
+					<button type="button" className="btn btn-primary form-control" onClick={saveContact()}>
 						save
 					</button>
 					<Link className="mt-3 w-100 text-center" to="/">
